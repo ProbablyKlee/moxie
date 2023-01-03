11ecdde14a3aa7448f4c90a6901a15d6210bff29
Lia Marie <lia.thaila.students@gmail.com>
2023-01-03 14:29:37 +0100

    Add project constants

---
+++ src/constants/baseclass.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+
+"""
+The MIT License (MIT)
+Copyright (c) 2022-Present Lia Marie
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+"""
+
+__all__ = ("CONSTANTS",)
+
+
+class ConstantsMeta(type):
+    @staticmethod
+    def is_valid_constant(attr_name):
+        return attr_name.isupper() and all(c.isalpha() or c == '_' for c in attr_name)
+
+    def __new__(mcs, name, bases, attrs):
+        """Ensures that all constants are valid."""
+        for attr_name, attr_value in attrs.items():
+            if attr_name.startswith('__') and attr_name.endswith('__') or isinstance(attr_value, property):
+                continue
+
+            if not mcs.is_valid_constant(attr_name):
+                raise ValueError(f"Attribute <{attr_name}> is not a valid constant")
+
+        return super().__new__(mcs, name, bases, attrs)
+
+    def __setattr__(self, attr, nv):
+        raise RuntimeError(f"Cannot assign to attribute <{attr}> of Constant object.")
+
+    def __delattr__(self, attr):
+        raise RuntimeError(f"Cannot delete attribute <{attr}> of Constant object")
+
+    def __getitem__(self, attr):
+        if hasattr(self, attr):
+            return getattr(self, attr)
+        raise KeyError(f"<{attr}> is not a valid key for Constant object")
+
+    def __iter__(self):
+        for attr in dir(self):
+            if not attr.startswith('_'):
+                yield attr
+
+    def __len__(self):
+        return sum(1 for _ in self)
+
+    def __contains__(self, attr):
+        return hasattr(self, attr)
+
+
+class CONSTANTS(metaclass=ConstantsMeta):
+    """Baseclass for constants.
+
+    - This class serves as a base for defining constants in a subclass.
+    - Constants defined in a subclass cannot be modified or deleted at runtime.
+    - Constants defined in a subclass can be accessed as attributes or as dictionary keys.
+
+    Examples
+    --------
+    >>> class MyConstants(CONSTANTS):
+    ...     FOO = 'bar'
+    ...
+    ... constants = MyConstants()
+    ... constants.FOO = 'baz'  # Raises a RuntimeError: 'Cannot assign to attribute <FOO> of Constant object.'
+    ... del constants.FOO      # Raises a RuntimeError: 'Cannot delete attribute <FOO> of Constant object'
+    ... constants.BAZ          # Raises an AttributeError: 'MyConstants' object has no attribute 'BAZ'
+    ... 'FOO' in constants     # True
+    ... 'BAZ' in constants     # False
+    """
+
+    pass  # The body of the class would go here, including any attributes and methods
---
+++ src/constants/constants.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+
+"""
+The MIT License (MIT)
+Copyright (c) 2022-Present Lia Marie
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+"""
+import regex
+
+from discord import Object
+from discord.abc import Snowflake
+
+from .baseclass import CONSTANTS
+
+__all__ = ('Colours', 'RMoxie', 'Ext', 'Regex', 'Bots', 'Emojis')
+
+
+class Colours(CONSTANTS):
+
+    EMBED: int = 0xFFCCB4
+
+
+class RMoxie(CONSTANTS):
+
+    BOT_ID: int = 1054123882384212078
+    GUILD_ID: int = 802227019203084298
+
+    @property
+    def thumb_url(self) -> str:
+        return self.avatar_url  # maybe change this later
+
+    @property
+    def avatar_url(self) -> str:
+        return f"https://cdn.discordapp.com/attachments/1059817715583430667/1059825225451184228/rmoxie_av.png"
+
+    @property
+    def invite_url(self) -> str:
+        return (
+            f"https://discord.com/api/oauth2/authorize?"
+            f"client_id={self.BOT_ID}&permissions=8&scope=bot%20applications.commands"
+        )
+
+    @property
+    def guild_object(self) -> Snowflake:
+        return Object(id=self.GUILD_ID)
+
+
+class Regex(CONSTANTS):
+
+    EMOJI: regex.Pattern = regex.compile(r'((?<!<a?))?:(?P<name>\w+):(?(1)|(?!\d+>))')
+    MARKDOWN: regex.Pattern = regex.compile(r'(?<!`)(`+)(?!`)([\\s\\S]+?)(?<!`)\\1(?!`)')
+
+
+class Bots(CONSTANTS):
+
+    OWO: int = 408785106942164992
+
+
+class Emojis(CONSTANTS):
+
+    STOP: str = "<:please_stop:1057473529437765653>"
+
+
+class Ext(CONSTANTS):
+
+    ...
