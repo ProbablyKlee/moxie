5b7813062a7032caac5133811f0032b8a29d57f9
Lia Marie <lia.thaila.students@gmail.com>
2023-01-08 19:45:36 +0100

    update meths

--- src/models/user.py
+++ src/models/user.py
@@ -37,8 +37,8 @@ class User:
         self.user_id = record["user_id"]
         self.emoji_server_id = record["emoji_server_id"]
 
-    @classmethod
-    async def insert_maybe_user(cls, user_id: int, bot: RoboMoxie) -> None:
+    @staticmethod
+    async def insert_maybe_user(user_id: int, bot: RoboMoxie) -> None:
         await bot.db.fetch(
             """
             INSERT INTO users (user_id)
@@ -49,8 +49,8 @@ class User:
             simple=True,
         )
 
-    @classmethod
-    async def insert_many(cls, users: Sequence[discord.Member], bot: RoboMoxie) -> None:
+    @staticmethod
+    async def insert_many(users: Sequence[discord.Member], bot: RoboMoxie) -> None:
         return await bot.db.execute_many(
             """
             INSERT INTO users (user_id)
@@ -60,12 +60,12 @@ class User:
             ((u.id,) for u in users),
         )
 
-    @classmethod
-    async def insert_history_item(cls, user: discord.Member, user_type: str, entry_type: str, bot: RoboMoxie) -> None:
+    @staticmethod
+    async def insert_history_item(user: discord.Member, user_type: str, entry_type: str, bot: RoboMoxie) -> None:
         await bot.db.execute("SELECT insert_history_item($1, $2, $3);", user.id, user_type, entry_type)
 
-    @classmethod
-    async def insert_avatar_history_item(cls, user: discord.Member, p_format: str, avatar: bytes, bot: RoboMoxie) -> None:
+    @staticmethod
+    async def insert_avatar_history_item(user: discord.Member, p_format: str, avatar: bytes, bot: RoboMoxie) -> None:
         await bot.db.execute("SELECT insert_avatar_history_item($1, $2, $3, $4);", user.id, p_format, avatar)
 
     async def fetch_history(self, user_type: str) -> list[dict[str, ...]]:
